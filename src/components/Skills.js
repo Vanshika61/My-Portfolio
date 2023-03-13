87ac656edf9c292d306523e4c0b299136274bb40
Vanshika <agrawal10614@gmail.com>
2023-03-13 22:08:36 +0530

    "modified skill section"

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,32 +4,18 @@ export default function Skills() {
   return (
     <div>
         <h2 className='heading'>SKILLS</h2>
-      <div className="container" id='skills_section'>
-        <div className='icons center'>
-                <img src="https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://cdn.iconscout.com/icon/free/png-256/css-alt-3628710-3029935.png" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://img.icons8.com/color/256/git.png" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="" />
-            {/* </div>
-            <div className="icons"> */}
-                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="" />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png" alt="" />
-                <img src="https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667" alt="" />
+      <div className="container icons text-center" id='skills_section'>
+        <div className='icons text-center'>
+          <img src="https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png" alt="" />
+          <img src="https://cdn.iconscout.com/icon/free/png-256/css-alt-3628710-3029935.png" alt="" />
+          <img height="186px" src="https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/180px-Bootstrap_logo.svg.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png" alt="" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png" alt="" />
+          <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="" />
         </div>
       </div>
     </div>
